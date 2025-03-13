3f898869164e024a523194e9ac6fad66b5ab9ddd
Muhammad Nour <acadamiclearner@gmail.com>
2025-03-13 17:38:22 +0200

    create pages

--- gallery-client/src/App.jsx
+++ gallery-client/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
   
   return (
-    <>
-    </>
+    <BrowserRouter>
+      {/* <nav>This is my navbar</nav> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
