ed46b092b228fbb1a2c398ac0a2fdc90a6f74a87
Muhammad Nour <acadamiclearner@gmail.com>
2025-03-16 17:08:02 +0200

    create PhotoTagSkeleton

---
+++ gallery-server/models/PhotoTagSkeleton.php
@@ -0,0 +1,12 @@
+<?php
+class PhotoTagSkeleton
+{
+    public int $photo_id;
+    public int $tag_id;
+
+    public function __construct(int $photo_id, int $tag_id)
+    {
+        $this->photo_id = $photo_id;
+        $this->tag_id = $tag_id;
+    }
+}
