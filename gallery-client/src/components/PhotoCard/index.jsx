b619d6b00a2803befc953b3a0c2abc202550e081
Muhammad Nour <acadamiclearner@gmail.com>
2025-03-16 17:01:21 +0200

    handle photo deletion

--- gallery-client/src/components/PhotoCard/index.jsx
+++ gallery-client/src/components/PhotoCard/index.jsx
@@ -3,13 +3,17 @@ import './style.css'
 
 const BASE_URL = 'http://localhost:3000'
 
-const PhotoCard = ({ url, title, desc, handleDeletion }) => {
+const PhotoCard = ({ url, title, desc, handleDeletion, handleAddTag }) => {
     return (
         <div className='photo-card'>
+            <span>tag1 </span>
+            <span>tag2 </span>
+            <span>tag3 </span>
             <img src={`${BASE_URL}${url}`} alt={title || "Photo"} />
             <p>{title}</p>
             <p>{desc}</p>
             <button onClick={handleDeletion}>delete</button>
+            <button onClick={handleAddTag}>add tag</button>
         </div>
     )
 }
--- gallery-client/src/pages/Home/index.jsx
+++ gallery-client/src/pages/Home/index.jsx
@@ -56,6 +56,67 @@ const Home = () => {
 
 
 
+
+
+  // gen photos
+  const genPhotos = () => {
+    useEffect(() => {
+      const a = async () => {
+        try {
+          const res = await request("post", "get-photos", { owner: username });
+          console.log(res);
+          setPhotos(res); // Ensure res.data is an array
+        } catch (error) {
+          setPhotos([])
+        }
+      }
+      a()
+    }, [photosUpdated])
+
+
+    if (photos.length === 0) 
+      return (<p>no images to show</p>)
+    else
+      return (
+        <>
+          {photos.map(photo => (
+            <PhotoCard key={photo.id}
+              url={photo.url}
+              title={photo.title}
+              desc={photo.desc}
+              handleDeletion={() => handlePhotoDeletion(photo.id)} />
+          ))}
+        </>
+      );
+  }
+  // add photo
+  const uploadPhoto = async () => {
+
+    const base64 = await fileToBase64(photoFile)
+    request("post", "upload-photo", { "title": photoTitle, "desc": photoDesc, "base64": base64, "owner": username });
+    setPhotosUpdated(prev => !prev)
+  }
+
+  // delete photo
+  const handlePhotoDeletion = (id) => {
+    request("delete", "delete-photo", { id });
+    setPhotosUpdated(prev => !prev)
+  }
+
+
+  const handleSetPhotoFile = e => {
+    const file = e.target.files[0]; // Get the selected file
+
+    if (!file) {
+      console.error("No file selected");
+      return; // Exit if no file is selected
+    }
+    console.log(file)
+    setPhotoFile(file)
+  }
+
+
+  // tags gen
   const tagsGen = () => {
     useEffect(() => {
       const a = async () => {
@@ -89,35 +150,6 @@ const Home = () => {
       </>
     );
   };
-
-  // gen photos
-
-
-
-  const genPhotos = () => {
-    useEffect(() => {
-      const a = async () => {
-        try {
-          const res = await request("post", "get-photos", { owner: username });
-          console.log(res);
-          setPhotos(res); // Ensure res.data is an array
-        } catch (error) {
-          setPhotos([])
-        }
-      }
-      a()
-    }, [photosUpdated])
-
-    return (
-      <>
-        {photos.map(photo => (
-          <PhotoCard key={photo.id} url={photo.url} title={photo.title} desc={photo.desc} />
-        ))}
-      </>
-    );
-  }
-
-
   // add tag
   const addTag = () => {
     request("post", "create-tag", { "name": tagName, "color": tagColor, "owner": username });
@@ -131,27 +163,8 @@ const Home = () => {
 
 
 
-  // add photo
 
 
-
-  const uploadPhoto = async () => {
-
-    const base64 = await fileToBase64(photoFile)
-    request("post", "upload-photo", { "title": photoTitle, "desc": photoDesc, "base64": base64, "owner": username });
-    setPhotosUpdated(prev => !prev)
-  }
-
-  const handleSetPhotoFile = e => {
-    const file = e.target.files[0]; // Get the selected file
-
-    if (!file) {
-      console.error("No file selected");
-      return; // Exit if no file is selected
-    }
-    console.log(file)
-    setPhotoFile(file)
-  }
   return (
     <div>
 
